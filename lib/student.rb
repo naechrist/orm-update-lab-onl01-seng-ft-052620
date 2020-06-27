0a5870c4c5e10ec5e4bd137cfd0ca95f6117a075
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-27 00:22:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,10 @@ class Student
   end
   
   def self.new_from_db(row)
-    student = self.new 
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 end
